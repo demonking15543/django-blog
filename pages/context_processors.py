f907aff6dc63aaf8c75c037f8b2561ef6ef2ac28
demonking15543 <demonking15543@gmail.com>
2022-11-14 23:23:28 +0530

    contact_processor updated

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -2,10 +2,20 @@ from .models import Post
 
 
 def my_blogs(request):
-    my_blogs = Post.objects.filter(author=request.user)
-    if len(my_blogs) > 3:
-        my_blogs=my_blogs[::3]
-
-    return {
+    try:
+        my_blogs = Post.objects.filter(author=request.user)
+        if len(my_blogs) > 3:
+            my_blogs=my_blogs[::3]
+        return {
         'my_blogs': my_blogs,
     }
+    
+    except Exception:
+        return {
+            'my_blogs':'',
+
+        }
+
+
+
+
