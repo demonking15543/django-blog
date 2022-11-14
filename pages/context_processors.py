3fa50e8689a17b369f91a1f661d8171655fc4256
demonking15543 <demonking15543@gmail.com>
2022-11-14 23:58:44 +0530

    context processor updated

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -5,7 +5,8 @@ def my_blogs(request):
     try:
         my_blogs = Post.objects.filter(author=request.user)
         if len(my_blogs) > 3:
-            my_blogs=my_blogs[::3]
+            my_blogs=my_blogs[:3]
+            print(my_blogs)
         return {
         'my_blogs': my_blogs,
     }
