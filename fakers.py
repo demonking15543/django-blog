2e2b35b71c486205969cd767fef47360d7dc6faa
demonking15543 <demonking15543@gmail.com>
2022-11-15 14:53:15 +0530

    faker updated

--- fakers.py
+++ fakers.py
@@ -3,8 +3,9 @@ from pages.models import Post
 
 fake = Faker()
 def gen_fake(num):
+    title=fake.name()
     Post.objects.bulk_create(
-        [Post(title=fake.name(), description=fake.text(), status="Publish", author_id=1, slug=str(fake.name()).replace(" ", "-")) for _ in range(num)]
+        [Post(title=title, description=fake.text(), status="Publish", author_id=1, slug=str(title).replace(" ", "-")) for _ in range(num)]
     )
     print("success")
     return
